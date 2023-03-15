b02b79a90d2cdb00ffff0167d5010b5ff7564fe0
LexBondarchyk <108199972+LexBondarchyk@users.noreply.github.com>
2023-03-15 10:37:46 +0200

    up

--- src/components/ContactForm/fields.js
+++ src/components/ContactForm/fields.js
@@ -4,7 +4,7 @@ const fields = {
     name: 'name',
     label: 'Name',
     placeholder: 'User name',
-    // pattern: "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$",
+ 
     title:
       "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan",
   },
@@ -13,7 +13,7 @@ const fields = {
     name: 'number',
     label: 'Number',
     placeholder: '444-44-44',
-    // pattern: '+?d{1,4}?[-.s]?(?d{1,3}?)?[-.s]?d{1,4}[-.s]?d{1,4}[-.s]?d{1,9}',
+   
     title:
       'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +',
   },
